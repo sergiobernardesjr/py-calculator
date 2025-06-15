9dc1620c48181176c8c481379a0b7da39f433b8a
sergio.bernardes <sergio.bernardes@cwi.com.br>
2025-06-15 13:39:16 -0300

    feat :sparkles: Adicionado maneira para sair

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-n1 = float(input("Digite um número: "))
-n2 = float(input("Digite outro número: "))
-op = input("Digite a operação desejada: ")
+def sum(n1, n2):
+  return n1 + n2
 
-if op == '/':
-  resultado = n1/n2
+def sub(n1, n2):
+  return n1 - n2
 
-if op == '-':
-  resultado = n1 - n2
+def split(n1, n2):
+  return n1 / n2
 
-if op == '+':
-  resultado = n1 + n2
+def mult(n1, n2):
+  return n1 * n2
 
-if op == '*':
-  resultado = n1 * n2
+while True:
 
-print(f'Resultado {str(resultado)}')
+  op = input("Digite a operação desejada ou 'sair' para sair: ").strip().lower()
+  
+  if op == 'sair':
+    break
+ 
+  n1 = float(input("Digite um número: "))
+  n2 = float(input("Digite outro número: "))
+
+  if op == '/':
+    resultado = split(n1,n2)
+
+  if op == '-':
+    resultado = sub(n1,n2)
+
+  if op == '+':
+    resultado = sum(n1,n2)
+
+  if op == '*':
+    resultado = mult(n1,n2)
+
+
+
+  print(f'Resultado {str(resultado)}')
