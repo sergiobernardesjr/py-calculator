0c422e49b7e3a5a4826dfdabd3751447a8ab3c72
sergio.bernardes <sergio.bernardes@cwi.com.br>
2025-06-15 13:19:58 -0300

    feat :sparkles: Calculadora com funcionamento operacional básico.

---
+++ main.py
@@ -0,0 +1,17 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+op = input("Digite a operação desejada: ")
+
+if op == '/':
+  resultado = n1/n2
+
+if op == '-':
+  resultado = n1 - n2
+
+if op == '+':
+  resultado = n1 + n2
+
+if op == '*':
+  resultado = n1 * n2
+
+print(f'Resultado {str(resultado)}')
